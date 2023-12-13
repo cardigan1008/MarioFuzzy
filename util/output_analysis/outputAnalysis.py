73d54e4f490093308494cdc7504a16fac179c3b4
86150 <2041733900@qq.com>
2023-12-13 13:42:10 +0800

    提取mario特征

--- util/output_analysis/outputAnalysis.py
+++ util/output_analysis/outputAnalysis.py
@@ -6,6 +6,8 @@ import cv2
 import win32api, win32con, win32gui
 from PIL import ImageGrab
 from numberExtract import number_get
+import pickle
+
 
 def get_window_pos(name):
     name = name
@@ -28,6 +30,18 @@ def fetch_image():
     grab_image = ImageGrab.grab((x1 + 20, y1, x2 - 20, y2-20))
 
     return grab_image
+
+
+def save_data(file_path, data):
+    with open(file_path, 'wb') as file:
+        pickle.dump(data, file)
+    print(f"result saved to {file_path}")
+
+
+def load_des(file_path):
+    with open(file_path, 'rb') as file:
+        data = pickle.load(file)
+    return data
 
 
 class OutputAnalysis:
@@ -57,9 +71,12 @@ class OutputAnalysis:
             arr_des.append(des1)
             arr_kp.append(kp1)
             arr_template.append(template)
+        save_data('train_des.pkl', arr_des)
         return arr_des, arr_kp, arr_template
 
-    def check_image(self, image_path, train_des, train_kp, train_template):
+
+    def check_image(self, image_path):
+        train_des = load_des('train_des.pkl')
         screenshot = cv2.imread(image_path, 0)
         if screenshot is None:
             print('Could not open or find the images!')
@@ -76,8 +93,8 @@ class OutputAnalysis:
             for m, n in matches:
                 if m.distance < 0.75 * n.distance:
                     good.append([m])
-            # 若匹配超过5点则认为存在
-            if len(good) > 5:
+            # 若匹配超过6点则认为存在
+            if len(good) > 6:
                 # # 画出匹配结果
                 # img3 = cv2.drawMatchesKnn(train_template[i], train_kp[i], screenshot, kp2, good, None, flags=2)
                 # # 显示图片
@@ -119,8 +136,9 @@ if __name__ == "__main__":
     s = fetch_image()
     s.save("screenshot.png")
     analysis = OutputAnalysis()
-    t1,t2,t3 = analysis.extract_mario_des()
-    analysis.check_image("screenshot.png",t1,t2,t3)
+    # 提取特征
+    # t1, t2, t3 = analysis.extract_mario_des()
+    analysis.check_image("screenshot.png")
     analysis.extract_score_from_image("screenshot.png")
     analysis.extract_gold_from_image("screenshot.png")
 
